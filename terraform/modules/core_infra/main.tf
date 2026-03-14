1ad4efdce2db5b4cb1e9ac62b92ecbb5cb783dff
andriiklykavkaa <andklykavka@gmail.com>
2026-03-15 00:26:27 +0200

    Terraform: Add deploy namespaces 2.0

--- terraform/modules/core_infra/main.tf
+++ terraform/modules/core_infra/main.tf
@@ -12,3 +12,18 @@ resource "google_project_service" "enabled_apis" {
   service            = each.key
   disable_on_destroy = false
 }
+
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.core_infra.cluster_endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.core_infra.cluster_ca_certificate)
+}
+
+resource "kubernetes_namespace" "retail_store" {
+  metadata {
+    name = "retail-store-${var.env_name}" 
+  }
+}
