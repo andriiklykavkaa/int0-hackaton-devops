2b46ee492114dc8096d7ba3624b37473915e77f2
andriiklykavkaa <andklykavka@gmail.com>
2026-03-14 23:07:09 +0200

    Added ground for Ingress

---
+++ terraform/modules/core_infra/dns.tf
@@ -0,0 +1,20 @@
+
+resource "google_compute_global_address" "ingress_ip" {
+    name = "squad-${var.env_name}-static-ip"
+}
+
+resource "google_dns_managed_zone" "ecomerce_zone" {
+    name        = "squad-${var.env_name}-zone"
+    dns_name    = "squad-team.com."
+    description = "DNS zone for ${var.env_name} environment"
+}
+
+resource "google_dns_record_set" "a_record" {
+    name         = google_dns_managed_zone.ecommerce_zone.dns_name
+    managed_zone = google_dns_managed_zone.ecommerce_zone.name
+    type         = "A"
+    ttl          = 300
+    rrdatas      = [google_compute_global_address.ingress_ip.address]
+}
+
+
