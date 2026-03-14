d47701dc937a5361f2ef149e096d74b9438ccae3
andriiklykavkaa <andklykavka@gmail.com>
2026-03-14 23:33:04 +0200

    Terraform: Output display 2.0

--- terraform/environments/prod/outputs.tf
+++ terraform/environments/prod/outputs.tf
@@ -1,8 +1,8 @@
-output "stage_ingress_ip" {
+output "prod_ingress_ip" {
   description = "Static IP for Prod Ingress"
   value       = module.core_infra.ingress_static_ip 
 }
 
-output "stage_name_servers" {
+output "prod_name_servers" {
   value = module.core_infra.name_servers
 }
